0d391bc3dffbb87bab857be26cc649bf5df65834
Matteo Vaccari <mvaccari@thoughtworks.com>
2023-06-08 13:53:44 +0200

    Refactor attack command

--- src/model/commands.js
+++ src/model/commands.js
@@ -28,32 +28,39 @@ export class EndPhaseCommand {
 }
 
 export class AttackCommand {
-    constructor(to, from) {
-        this.to = to;
-        this.from = from;
+    constructor(toHex, fromHex) {
+        this.toHex = toHex;
+        this.fromHex = fromHex;
     }
 
     toString() {
-        return `Attack from ${this.from} to ${this.to}`;
+        return `Attack from ${this.fromHex} to ${this.toHex}`;
     }
 
     play(game) {
-        const attackingUnit = game.unitAt(this.from);
+        const attackingUnit = game.unitAt(this.fromHex);
         if (!attackingUnit) {
-            throw new Error(`No unit at ${this.from}`);
+            throw new Error(`No unit at ${this.fromHex}`);
         }
-        const defendingUnit = game.unitAt(this.to);
+        const defendingUnit = game.unitAt(this.toHex);
         if (!defendingUnit) {
-            throw new Error(`No unit at ${this.to}`);
+            throw new Error(`No unit at ${this.toHex}`);
         }
         if (attackingUnit.side === defendingUnit.side) {
-            throw new Error(`Cannot attack own unit at ${this.to}`);
+            throw new Error(`Cannot attack own unit at ${this.toHex}`);
+        }
+        if (this.toHex === this.fromHex) {
+            throw new Error(`Cannot attack self at ${this.toHex}`);
+        }
+        if (this.toHex.distance(this.fromHex) > 1) {
+            throw new Error(`Cannot attack unit at ${this.toHex} from ${this.fromHex} (too far)`);
         }
         const diceResults = game.roll(attackingUnit.diceCount());
         const damage = defendingUnit.takeDamage(diceResults);
         return [
-            new DamageEvent(this.to, damage, diceResults),
-            new BattleBackEvent(this.from, this.to, defendingUnit.diceCount())
+            new DamageEvent(this.toHex, damage, diceResults),
+            new BattleBackEvent(this.fromHex, this.toHex, defendingUnit.diceCount())
         ]
     }
 }
+
