981111bb05c56505c1c8623277090456cce5fe4e
Matteo Vaccari <mvaccari@thoughtworks.com>
2023-06-13 13:57:34 +0200

    We now have a mean AI

--- src/autoplay.js
+++ src/autoplay.js
@@ -5,7 +5,7 @@ import { redraw } from "./view/graphics.js";
 const AUTOPLAY_DELAY = 800;
 
 const textBox = document.getElementById("messages");
-export function displayEvents(events) {    
+export function displayEvents(events) {
     events.forEach(event => {
         textBox.innerHTML += `${event}<br/>`;
         textBox.scrollTop = textBox.scrollHeight;
@@ -21,13 +21,22 @@ export class Autoplay {
         while (this.game.currentSide === Side.CARTHAGINIAN) {
             let commands = this.game.validCommands();
             if (commands.length === 0) {
-                return ;
+                return;
             }
-            let command = commands[Math.floor(Math.random() * commands.length)];
-            console.log("Command:", command);
+            commands.forEach((command, index) => console.log(command.toString(), command.value(this.game)));
+            
+            // sort commands by value
+            commands.sort((a, b) => b.value(this.game) - a.value(this.game));
+            
+            // extract all the commands with the highest value
+            let bestCommands = commands.filter(command => command.value(this.game) === commands[0].value(this.game));
+            
+            // choose randomly from the best commands
+            let command = bestCommands[Math.floor(Math.random() * bestCommands.length)];
+            
             let events = this.game.executeCommand(command);
             displayEvents(events);
-            redraw(ctx,graphics, this.game);
+            redraw(ctx, graphics, this.game);
             await new Promise(resolve => setTimeout(resolve, AUTOPLAY_DELAY));
         }
     }
