42526d6fc629f03a0597432dd1985991d4785461
Matteo Vaccari <vaccari@pobox.com>
2023-07-04 15:23:37 +0200

    Optimize: avoid recomputing Math.pow many times

--- src/model/commands/commands.js
+++ src/model/commands/commands.js
@@ -2,6 +2,16 @@ import { RESULT_FLAG } from "../dice.js";
 
 const DISTANCE_VALUE_BACKOFF = 0.2;
 
+// Optimization: precompute the values for the backoff function
+function backoffValues(number) {
+    let values = [];
+    for (let i = 0; i < number; i++) {
+        values.push(Math.pow(DISTANCE_VALUE_BACKOFF, i));
+    }
+    return values;
+}
+const BACKOFF_VALUES = backoffValues(17);
+
 /*
     Commands are immutable objects representing the actions that can be taken by the player. 
     They are used by both the human player and the AI to execute a move.
@@ -13,7 +23,7 @@ export function hexScore(enemyUnitStrength) {
 
 function valueOfHex(hexToBeEvaluated, enemyUnitHex, enemyUnitStrength) {
     const distance = enemyUnitHex.distance(hexToBeEvaluated);
-    return hexScore(enemyUnitStrength) * Math.pow(DISTANCE_VALUE_BACKOFF, distance);
+    return hexScore(enemyUnitStrength) * BACKOFF_VALUES[distance];
 }
 
 export function valueOfHexOverAllEnemyUnits(game, hexToBeEvaluated, friendlySide) {
