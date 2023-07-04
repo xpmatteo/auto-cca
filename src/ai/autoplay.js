cd10906192f7f77681c1375d53b20540c23b4f3d
Matteo Vaccari <vaccari@pobox.com>
2023-07-04 16:51:44 +0200

    Make AI play continuous

--- src/ai/autoplay.js
+++ src/ai/autoplay.js
@@ -75,7 +75,7 @@ export class Autoplay {
     }
 
     async play(graphics) {
-        if (this.game.currentSide === Side.CARTHAGINIAN && !this.game.isTerminal()) {
+        while (this.game.currentSide === Side.CARTHAGINIAN && !this.game.isTerminal()) {
             const commands = this.aiPlayer.decideMove(this.game);
             for (let command of commands) {
                 console.log("Executing command: " + command);
