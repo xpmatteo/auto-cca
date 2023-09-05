7d58046d10845f047c1ebbcf96fbd1bc037734bd
Matteo Vaccari <vaccari@pobox.com>
2023-09-05 16:22:32 +0200

    Rudimentary score function

--- src/ai/score.js
+++ src/ai/score.js
@@ -1,10 +1,22 @@
+const SCORE_FOR_SUPPORTED_UNIT = 1;
+const SCORE_FOR_EACH_DIE = 1;
+
+function decideDiceCountForRanged(hex, game) {
+    return game.unitHasMoved(hex) ? 1 : 2;
+}
 
 export function score(game, side) {
     let score = 0;
     game.foreachUnitOfSide(side, (unit, hex) => {
         if (game.isSupported(hex)) {
-            score += 1;
+            score += SCORE_FOR_SUPPORTED_UNIT;
         }
+        unit.validCloseCombatTargets(hex, game).forEach(to => {
+            score += unit.diceCount * SCORE_FOR_EACH_DIE;
+        });
+        unit.validRangedCombatTargets(hex, game).forEach(to => {
+            score += decideDiceCountForRanged(hex, game) * SCORE_FOR_EACH_DIE;
+        });
     });
 
     return score;
--- test/ai/score_test.js
+++ test/ai/score_test.js
@@ -3,10 +3,7 @@ import makeGame from "model/game.js";
 import { Side } from "model/side.js";
 import { score } from "ai/score.js";
 import { hexOf } from "xlib/hexlib.js";
-import { RomanLightInfantry } from "model/units.js";
-
-// 1 point for every unit with support
-// 1 point for every dice you can roll
+import { CarthaginianHeavyInfantry, RomanLightInfantry } from "model/units.js";
 
 
 test("default score is zero", () => {
@@ -17,12 +14,42 @@ test("default score is zero", () => {
 });
 
 test("score one point for every unit with support", () => {
-   const game = makeGame(new NullScenario());
-
+    const game = makeGame(new NullScenario());
     game.placeUnit(hexOf(0, 0), new RomanLightInfantry()); // no support
     game.placeUnit(hexOf(1, 0), new RomanLightInfantry()); // support
     game.placeUnit(hexOf(2, 0), new RomanLightInfantry()); // support
     game.placeUnit(hexOf(3, 0), new RomanLightInfantry()); // no support
 
     expect(score(game, Side.ROMAN)).toBe(2);
+    expect(score(game, Side.CARTHAGINIAN)).toBe(0);
 });
+
+test("score one point for every die you can roll in close combat", () => {
+    const game = makeGame(new NullScenario());
+    game.placeUnit(hexOf(0, 0), new RomanLightInfantry()); // two dice
+    game.placeUnit(hexOf(1, 0), new CarthaginianHeavyInfantry()); // five dice
+
+    expect(score(game, Side.ROMAN)).toBe(2);
+    expect(score(game, Side.CARTHAGINIAN)).toBe(5);
+});
+
+test("score one point for every die you can roll in ranged combat", () => {
+    const game = makeGame(new NullScenario());
+    game.placeUnit(hexOf(0, 0), new RomanLightInfantry()); // two dice
+    game.placeUnit(hexOf(2, 0), new CarthaginianHeavyInfantry()); // no ranged combat
+
+    expect(score(game, Side.ROMAN)).toBe(2);
+    expect(score(game, Side.CARTHAGINIAN)).toBe(0);
+});
+
+test("take into account that units that moved have fewer dice", () => {
+    const game = makeGame(new NullScenario());
+    game.placeUnit(hexOf(0, 0), new RomanLightInfantry()); // two dice
+    game.addMovementTrail(hexOf(0, 0), hexOf(1, 0)); // moved, so only one die
+    game.placeUnit(hexOf(2, 0), new CarthaginianHeavyInfantry()); // no ranged combat
+
+    expect(score(game, Side.ROMAN)).toBe(1);
+    expect(score(game, Side.CARTHAGINIAN)).toBe(0);
+});
+
+
