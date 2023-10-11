3d0eafbac513d96539f2dc3bc2f955817b075699
Matteo Vaccari <vaccari@pobox.com>
2023-10-11 18:47:35 +0200

    Remove old nondet algo

--- test/ai/mcts_player_test.js
+++ test/ai/mcts_player_test.js
@@ -1,4 +1,4 @@
-import { executeCommandManyTimes, MctsPlayer, TreeNode } from "ai/mcts_player.js";
+import { MctsPlayer, TreeNode } from "ai/mcts_player.js";
 import { OrderHeavyTroopsCard } from "model/cards.js";
 import { CloseCombatCommand } from "model/commands/close_combat_command.js";
 import { RangedCombatCommand } from "model/commands/ranged_combat_command.js";
@@ -43,41 +43,42 @@ function diceReturningSequence(...results) {
     }
 }
 
-test('select the best uct child and then battles', () => {
-    const player = new MctsPlayer({nonDeterministicCommandRepetitions: 10000});
-    const game = makeGame(new NullScenario());
-    game.placeUnit(hexOf(0, 0), new RomanHeavyInfantry());
-    game.placeUnit(hexOf(1, 0), new CarthaginianMediumInfantry());
-    game.handSouth = [new OrderHeavyTroopsCard()];
-    const root = new TreeNode(game);
-
-    player.iterate(root); // play card
-    player.iterate(root); // end phase
-    player.iterate(root); // move
-    player.iterate(root); // end phase
-    player.iterate(root); // close combat
-
-    // console.log(root.toString(4))
-    //     43/7: undefined -> Roman play one card -> 1
-    //      43/7: PlayCard(Order Heavy Troops) -> Roman order 1 heavy units -> 1
-    //       43/6: End phase -> Roman movement -> 2
-    //        0/2: Move [0,0] to [0,1] -> Roman movement -> 1
-    //         0/1: End phase -> Roman battle -> 0
-    //        43/3: End phase -> Roman battle -> 2
-    //         43/1: Close Combat from [0,0] to [1,0] -> Roman battle -> 0
-    //         0/1: End phase -> Carthaginian play one card -> 0
-    expect(root.describeNode()).toBe("30/7: undefined -> Roman play one card -> 1");
-    expect(root.children[0].describeNode()).toBe("30/7: PlayCard\(Order Heavy Troops\) -> Roman order 1 heavy units -> 1");
-    expect(root.children[0].children[0].describeNode()).toBe("30/6: End phase -> Roman movement -> 2");
-    expect(root.children[0].children[0].children[0].describeNode()).toBe("0/2: Move [0,0] to [0,1] -> Roman movement -> 1");
-    expect(root.children[0].children[0].children[0].children[0].describeNode()).toBe("0/1: End phase -> Roman battle -> 0");
-    expect(root.children[0].children[0].children[1].describeNode()).toBe("30/3: End phase -> Roman battle -> 2");
-    expect(root.children[0].children[0].children[1].children[0].describeNode()).toMatch(/30\/1: Close Combat from \[0,0\] to \[1,0\] -> Roman (battle|retreat) -> 0/);
-    expect(root.children[0].children[0].children[1].children[1].describeNode()).toBe("0/1: End phase -> Carthaginian play one card -> 0");
-});
-
 describe('Non deterministic results', () => {
-    test('close combat from light to heavy', () => {
+    xtest('select the best uct child and then battles', () => {
+        const player = new MctsPlayer({nonDeterministicCommandRepetitions: 10000});
+        const game = makeGame(new NullScenario());
+        game.placeUnit(hexOf(0, 0), new RomanHeavyInfantry());
+        game.placeUnit(hexOf(1, 0), new CarthaginianMediumInfantry());
+        game.handSouth = [new OrderHeavyTroopsCard()];
+        const root = new TreeNode(game);
+
+        player.iterate(root); // play card
+        player.iterate(root); // end phase
+        player.iterate(root); // move
+        player.iterate(root); // end phase
+        player.iterate(root); // close combat
+
+        // console.log(root.toString(4))
+        //     43/7: undefined -> Roman play one card -> 1
+        //      43/7: PlayCard(Order Heavy Troops) -> Roman order 1 heavy units -> 1
+        //       43/6: End phase -> Roman movement -> 2
+        //        0/2: Move [0,0] to [0,1] -> Roman movement -> 1
+        //         0/1: End phase -> Roman battle -> 0
+        //        43/3: End phase -> Roman battle -> 2
+        //         43/1: Close Combat from [0,0] to [1,0] -> Roman battle -> 0
+        //         0/1: End phase -> Carthaginian play one card -> 0
+        expect(root.describeNode()).toBe("30/7: undefined -> Roman play one card -> 1");
+        expect(root.children[0].describeNode()).toBe("30/7: PlayCard\(Order Heavy Troops\) -> Roman order 1 heavy units -> 1");
+        expect(root.children[0].children[0].describeNode()).toBe("30/6: End phase -> Roman movement -> 2");
+        expect(root.children[0].children[0].children[0].describeNode()).toBe("0/2: Move [0,0] to [0,1] -> Roman movement -> 1");
+        expect(root.children[0].children[0].children[0].children[0].describeNode()).toBe("0/1: End phase -> Roman battle -> 0");
+        expect(root.children[0].children[0].children[1].describeNode()).toBe("30/3: End phase -> Roman battle -> 2");
+        expect(root.children[0].children[0].children[1].children[0].describeNode()).toMatch(/30\/1: Close Combat from \[0,0\] to \[1,0\] -> Roman (battle|retreat) -> 0/);
+        expect(root.children[0].children[0].children[1].children[1].describeNode()).toBe("0/1: End phase -> Carthaginian play one card -> 0");
+    });
+
+
+    xtest('close combat from light to heavy', () => {
         const game = makeGame(new NullScenario());
         game.placeUnit(hexOf(2, 2), new CarthaginianHeavyInfantry());
         game.placeUnit(hexOf(2, 3), new RomanLightInfantry());
@@ -95,7 +96,7 @@ describe('Non deterministic results', () => {
         }
     }
 
-    test('close combat from heavy to heavy', () => {
+    xtest('close combat from heavy to heavy', () => {
         const game = makeGame(new NullScenario());
         game.placeUnit(hexOf(2, 2), new CarthaginianHeavyInfantry());
         game.placeUnit(hexOf(2, 3), new RomanHeavyInfantry());
@@ -108,7 +109,7 @@ describe('Non deterministic results', () => {
         expect(nondeterministicResults.closestScoreToAverageScore()).toBe(20);
     });
 
-    test('ranged combat', () => {
+    xtest('ranged combat', () => {
         const game = makeGame(new NullScenario());
         game.placeUnit(hexOf(2, 2), new CarthaginianHeavyInfantry());
         game.placeUnit(hexOf(2, 4), new RomanLightInfantry());
@@ -120,5 +121,4 @@ describe('Non deterministic results', () => {
         expectBetween(nondeterministicResults.averageScore(), 9, 11);
         expect(nondeterministicResults.closestScoreToAverageScore()).toBe(10);
     });
-
 });
