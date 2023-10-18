0da87e2fbbd52edafe4891a33ea9f076c132ebe1
Matteo Vaccari <vaccari@pobox.com>
2023-10-18 14:07:43 +0200

    fix mcts tree tests

--- test/ai/mcts_player_test.js
+++ test/ai/mcts_player_test.js
@@ -1,10 +1,10 @@
-import { MctsPlayer, DecisionNode, ChanceNode } from "ai/mcts_player.js";
+import * as fs from "fs";
+import { DecisionNode, MctsPlayer } from "ai/mcts_player.js";
 import { OrderHeavyTroopsCard } from "model/cards.js";
 import { CloseCombatCommand } from "model/commands/close_combat_command.js";
 import { RangedCombatCommand } from "model/commands/ranged_combat_command.js";
-import { Dice, diceReturning, RESULT_FLAG, RESULT_HEAVY, RESULT_LIGHT, RESULT_MEDIUM } from "model/dice.js";
 import makeGame from "model/game.js";
-import { NullScenario } from "model/scenarios.js";
+import { AkragasScenario, MeleeScenario, NullScenario, TwoOnTwoMeleeScenario } from "model/scenarios.js";
 import {
     CarthaginianHeavyInfantry,
     CarthaginianMediumInfantry,
@@ -12,36 +12,77 @@ import {
     RomanLightInfantry
 } from "model/units.js";
 import { hexOf } from "xlib/hexlib.js";
+import { fixedRandom } from "xlib/random.js";
 
+/**
+ * @param {string} fileName
+ */
+function dumpTreeToFile(root, fileName) {
+    const logger = fs.createWriteStream(fileName, {
+        flags: 'w'
+    });
+    function write(str) {
+        logger.write(str);
+        logger.write("\n");
+    }
+    root.dumpTree(write);
+}
 
-it('expands one node', () => {
-    const player = new MctsPlayer();
-    const game = makeGame(new NullScenario());
-    game.placeUnit(hexOf(0, 0), new RomanHeavyInfantry());
-    game.handSouth = [new OrderHeavyTroopsCard()];
-    game.executeCommand(game.validCommands()[0]); // play order heavy troops card
-    game.executeCommand(game.validCommands()[0]); // end phase
-    const root = new DecisionNode(game);
-
-    player.iterate(root);
-
-    expect(root.describeNode()).toBe("0/1: undefined -> Roman movement -> 3");
-    expect(root.children[0].describeNode()).toMatch(/0\/[01]: Move \[0,0\] to \[1,0\] -> Roman movement -> 0/);
-    expect(root.children[1].describeNode()).toMatch(/0\/[01]: Move \[0,0\] to \[0,1\] -> Roman movement -> 0/);
-    expect(root.children[2].describeNode()).toMatch(/0\/[01]: End phase -> Roman battle -> 0/);
+const originalRandom = Math.random;
+beforeEach(() => {
+   Math.random = fixedRandom;
 });
 
-function diceReturningSequence(...results) {
-    let nextResultIndex = 0;
-    return {
-        roll: function (count) {
-            if (nextResultIndex === results.length) {
-                return results[results.length - 1];
-            }
-            return results[nextResultIndex++];
-        }
-    }
-}
+afterEach(() => {
+    Math.random = originalRandom;
+});
+
+test('2 on 2', () => {
+    const player = new MctsPlayer();
+    const game = makeGame(new TwoOnTwoMeleeScenario());
+
+    const root = player.search(game, 2000);
+
+    const rootAsString = root.toString(3);
+    expect(rootAsString).toBe(`1393/2000: undefined -> Roman play one card -> 1
+ 1393/2000: PlayCard(Order Heavy Troops) -> Roman order 3 heavy units -> 1
+  1393/1999: End phase -> Roman movement -> 11
+   1/21: Move [-1,6] to [-1,7] -> Roman movement -> 6
+   1/21: Move [0,6] to [1,5] -> Roman movement -> 7
+   148/232: Move [0,6] to [-1,7] -> Roman movement -> 6
+   65/122: Move [-1,6] to [0,5] -> Roman movement -> 6
+   1141/1434: Move [-1,6] to [-1,5] -> Roman movement -> 7
+   -1/18: Move [-1,6] to [-2,7] -> Roman movement -> 7
+   -1/17: Move [0,6] to [0,7] -> Roman movement -> 7
+   0/19: Move [-1,6] to [-2,6] -> Roman movement -> 7
+   0/19: End phase -> Roman battle -> 1
+   34/64: Move [0,6] to [1,6] -> Roman movement -> 7
+   5/31: Move [0,6] to [0,5] -> Roman movement -> 6\n`);
+});
+
+test('akragas', () => {
+    const player = new MctsPlayer();
+    const game = makeGame(new AkragasScenario());
+
+    const root = player.search(game, 2000);
+    //dumpTreeToFile(root, 'tree.txt');
+
+    const rootAsString = root.toString(2);
+    expect(rootAsString).toBe(`0/2000: undefined -> Syracusan play one card -> 3
+ 0/667: PlayCard(Order Light Troops) -> Syracusan order 6 light units -> 7
+  0/96: OrderUnit([-2,7],[-1,7],[5,7],[6,7],[3,6],[7,6]) -> Syracusan order 6 light units -> 1
+  0/95: OrderUnit([-2,7],[-1,7],[5,7],[6,7],[0,6],[7,6]) -> Syracusan order 6 light units -> 1
+  0/95: OrderUnit([-1,7],[5,7],[6,7],[0,6],[3,6],[7,6]) -> Syracusan order 6 light units -> 1
+  0/95: OrderUnit([-2,7],[-1,7],[6,7],[0,6],[3,6],[7,6]) -> Syracusan order 6 light units -> 1
+  0/96: OrderUnit([-2,7],[5,7],[6,7],[0,6],[3,6],[7,6]) -> Syracusan order 6 light units -> 1
+  0/95: OrderUnit([-2,7],[-1,7],[5,7],[0,6],[3,6],[7,6]) -> Syracusan order 6 light units -> 1
+  0/95: OrderUnit([-2,7],[-1,7],[5,7],[6,7],[0,6],[3,6]) -> Syracusan order 6 light units -> 1
+ 0/667: PlayCard(Order Heavy Troops) -> Syracusan order 6 heavy units -> 1
+  0/666: End phase -> Syracusan movement -> 16
+ 0/666: PlayCard(Order Medium Troops) -> Syracusan order 6 medium units -> 1
+  0/666: End phase -> Syracusan movement -> 15\n`);
+});
+
 
 describe('Non deterministic results', () => {
 
