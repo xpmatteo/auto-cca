05eacbb9613a41c039f95631906be221ba70c73c
Matteo Vaccari <vaccari@pobox.com>
2023-11-04 18:25:19 +0100

    Add test that breaks bc of the problem

--- src/ai/mcts_player.test.js
+++ src/ai/mcts_player.test.js
@@ -1,7 +1,7 @@
 import { MctsPlayer } from "./mcts_player.js";
 import * as fs from "fs";
 import makeGame from "../model/game.js";
-import { NullScenario, TwoOnTwoMeleeScenario } from "../model/scenarios.js";
+import { MeleeScenario, NullScenario, TwoOnTwoMeleeScenario } from "../model/scenarios.js";
 import { CarthaginianHeavyInfantry, RomanLightInfantry } from "../model/units.js";
 import { hexOf } from "../lib/hexlib.js";
 import { fixedRandom, resetFixedRandom } from "../lib/random.js";
@@ -44,6 +44,16 @@ test('2 on 2', () => {
     const rootAsString = root.toString(6);
     expect(rootAsString).toMatchSnapshot();
 });
+
+test('melee', () => {
+    const game = makeGame(new MeleeScenario());
+
+    const root = player.search(game, 2000);
+
+    const rootAsString = root.toString(6);
+    expect(rootAsString).toMatchSnapshot();
+});
+
 
 test('close combat from light to heavy', () => {
     const game = makeGame(new NullScenario());
