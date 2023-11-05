8f5ffd7d27416e05ce3e448c2fa4e189d6041027
Matteo Vaccari <vaccari@pobox.com>
2023-11-05 16:36:58 +0100

    remove dependency loop

--- src/model/phases/first_defender_evasion_phase.js
+++ src/model/phases/first_defender_evasion_phase.js
@@ -1,7 +1,4 @@
 import { Phase } from "./Phase.js";
-import {EndPhaseCommand} from "../commands/end_phase_command.js";
-import {CloseCombatCommand} from "../commands/close_combat_command.js";
-import { RangedCombatCommand } from "../commands/ranged_combat_command.js";
 
 
 export class FirstDefenderEvasionPhase extends Phase {
